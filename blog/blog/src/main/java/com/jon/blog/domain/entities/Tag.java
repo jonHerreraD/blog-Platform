ada7c91f5f2562c6b03d65994ed8c59267a5dbe7
Jonathan <diazjonathan3009@gmail.com>
2025-04-16 00:18:43 -0600

    Added relationships between entities

--- blog/blog/src/main/java/com/jon/blog/domain/entities/Tag.java
+++ blog/blog/src/main/java/com/jon/blog/domain/entities/Tag.java
@@ -3,7 +3,9 @@ package com.jon.blog.domain.entities;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -20,6 +22,10 @@ public class Tag {
 
     @Column(nullable = false, unique = true)
     private String name;
+
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Post> posts = new HashSet<>();
 
     @Override
     public boolean equals(Object o) {
