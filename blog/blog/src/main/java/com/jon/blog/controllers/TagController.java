6e1f087ef64d2dcd1718f1392fd4ea1e13273d55
Jonathan <diazjonathan3009@gmail.com>
2025-06-07 14:22:44 -0600

    Added create Tag Endpoint

--- blog/blog/src/main/java/com/jon/blog/controllers/TagController.java
+++ blog/blog/src/main/java/com/jon/blog/controllers/TagController.java
@@ -1,15 +1,15 @@
 package com.jon.blog.controllers;
 
 
+import com.jon.blog.domain.dtos.CreateTagsRequest;
 import com.jon.blog.domain.dtos.TagResponse;
 import com.jon.blog.domain.entities.Tag;
 import com.jon.blog.mappers.TagMapper;
 import com.jon.blog.services.TagService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +26,16 @@ public class TagController {
         List<Tag> tags = tagService.getTags();
         List<TagResponse> tagResponses = tags.stream().map(tagMapper::toTagResponse).toList();
         return ResponseEntity.ok(tagResponses);
+    }
+
+    @PostMapping
+    public ResponseEntity<List<TagResponse>> createTags(@RequestBody CreateTagsRequest createTagsRequest){
+
+        List<Tag> savedTags = tagService.createTags(createTagsRequest.getNames());
+        List<TagResponse> createdTagResponses = savedTags.stream().map(tagMapper::toTagResponse).toList();
+        return new ResponseEntity<>(
+                createdTagResponses,
+                HttpStatus.CREATED
+        );
     }
 }
--- blog/blog/src/main/java/com/jon/blog/services/TagService.java
+++ blog/blog/src/main/java/com/jon/blog/services/TagService.java
@@ -3,8 +3,10 @@ package com.jon.blog.services;
 import com.jon.blog.domain.entities.Tag;
 
 import java.util.List;
+import java.util.Set;
 
 public interface TagService {
 
     List<Tag> getTags();
+    List<Tag> createTags(Set<String> tagNames);
 }
